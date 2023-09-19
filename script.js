144b80b30670dd517bce5dbe43bd1b0214796d57
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-09-19 15:44:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,54 +4,24 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+// Create a reference to the 'players' array
+const team = players;
+
+// Create a copy of the 'players' array using the spread operator
+const team1 = [...players];
+
+// Create a copy of the 'person' object using object destructuring
+const cap1 = { ...person };
+
+// Test if the variables are working as expected
+console.log(team);  // Should print ["John", "Bob", "Alice", "Poppy"]
+console.log(team1); // Should print ["John", "Bob", "Alice", "Poppy"]
+console.log(cap1);  // Should print { name: "John Doe", age: 80 }
+
+
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-let arr = [
-    { id: 1, name: "john", age: "18", profession: "developer" },
-    { id: 2, name: "jack", age: "20", profession: "developer" },
-    { id: 3, name: "karen", age: "19", profession: "admin" },
-  ];
-  
-  function PrintDeveloper() {
-    //Write your code here , just console.log
-     arr.map((val)=>{
-        if(val.profession === "developer"){
-         console.log(val);
-        }
-    })
-}
-  function addData() {
-    //Write your code here, just console.log
-    let newarr={
-        id: 4, name: "Anuj", age: "28", profession: "Intern"
-               }
-               arr.push(newarr);
-    console.log(arr);
-  }
-  
-  function removeAdmin() {
-    //Write your code here, just console.log
-    arr.filter((val)=>{
-if(val.profession!="admin")
-{
-    console.log(val);
-}
-    })
 
-  }
-  
-  function concatenateArray() {
-    //Write your code here, just console.log
-    let newEmployee=[
-        {id: 10, name: "Sourabh", age: "30", profession: "Tester"},
-        {id: 12, name: "Nikhil", age: "25", profession: "Developer"},
-        {id: 15, name: "Ram", age: "26", profession: "Software Engineer"}
-];
-
-let add= arr.concat(newEmployee);
-console.log(add);
-
-  }
 window.players = players;
 window.person = person;
 window.team = team;
